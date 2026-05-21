f9522b93b905915f0abd84f958d56482848f830e
Kara Lee Carver <karasactualemail@gmail.com>
2026-05-21 17:26:07 -0600

    error handling, cleanup, organization page

--- src/models/projects.js
+++ src/models/projects.js
@@ -19,4 +19,24 @@ const getAllProjects = async() => {
     return result.rows;
 }
 
-export {getAllProjects} 
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          service_project_id,
+          organization_id,
+          title,
+          description,
+          address,
+          date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+export {getAllProjects, getProjectsByOrganizationId }; 
