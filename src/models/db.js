fbff875b59a6fbf9d705acdd507c2086c1911d96
Kara Lee Carver <karasactualemail@gmail.com>
2026-06-08 13:14:13 -0600

    why do you hate meee

--- src/models/db.js
+++ src/models/db.js
@@ -11,8 +11,8 @@ import { Pool } from 'pg';
  * The connection string format is:
  * postgresql://username:password@host:port/database
  */
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
   ssl: {rejectUnauthorized: false}
 });
 
